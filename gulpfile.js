534535002d85b3e1fa0f26ace12212d1a61847c3
Gregg Cowell <Gregg Cowell>
2016-02-02 20:19:27 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+process.env.DISABLE_NOTIFIER = true;
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    //mix.sass('app.scss', 'resources/assets/css');
+
+
+    mix.styles
+        ([
+
+            'libs/jquery-ui.css',
+            'libs/jquery-ui.structure.css',
+            'libs/jquery-ui.theme.css',
+            'libs/bootstrap.min.css',
+            'app.css'
+
+        ]);
+
+
+    mix.scripts
+        ([
+            'libs/jquery.min.js',
+            'libs/jquery-ui.min.js',
+            'libs/bootstrap.min.js',
+            'libs/html5.shiv.js',
+            'libs/respond.min.js'
+
+        ]);
+
+
+});
---
+++ resources/views/users/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+
+@section('content')
+
+<h1>{{$user->name}}'s Dashboard</h1>
+
+
+@stop
